63b4321e863b81c17776506d3a17c5380b149331
Priyanshujawra <priyanshukumar93861@gmail.com>
2025-04-11 12:43:46 +0530

    updated job.js

--- Backend/routes/Routes_job.js
+++ Backend/routes/Routes_job.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require("../model/Job");
+const Job = require("../model/job");
 
 // Get all jobs
 router.get("/", async (req, res) => {
